4d1b643d4840d24285dcf25d7c11191c2d9bc67f
sreyashi-nag <sreyashn@andrew.cmu.edu>
2018-11-26 02:39:46 +0000

    fixing srl configs

--- taskonomy_config/srl_1k_elmo_wt.sh
+++ taskonomy_config/srl_1k_elmo_wt.sh
@@ -1,3 +1,2 @@
 source activate allennlp
-SRL_TRAIN_DATA_PATH=datasets/cross-task-subsets/srl_onto_1k.pkl SRL_VAL_DATA_PATH=datasets/conll-formatted-ontonotes-5.0/data/development/ allennlp train -s saved_models/srl_1k_elmo_wt
-taskonomy_config/srl_1k_elmo_wt.jsonnet
+SRL_TRAIN_DATA_PATH=datasets/cross-task-subsets/srl_onto_1k.pkl SRL_VAL_DATA_PATH=datasets/conll-formatted-ontonotes-5.0/data/development/ allennlp train -s saved_models/srl_1k_elmo_wt taskonomy_config/srl_1k_elmo_wt.jsonnet
--- taskonomy_config/srl_5k_glove_wt.sh
+++ taskonomy_config/srl_5k_glove_wt.sh
@@ -1,3 +1,2 @@
-source activate allennlp
-SRL_TRAIN_DATA_PATH=datasets/cross-task-subsets/srl_onto_5k.pkl SRL_VAL_DATA_PATH=datasets/conll-formatted-ontonotes-5.0/data/development/ allennlp train -s saved_models/srl_5k_glove_wt
-taskonomy_config/srl_5k_glove_wt.jsonnet
+#source activate allennlp
+SRL_TRAIN_DATA_PATH=datasets/cross-task-subsets/srl_onto_5k.pkl SRL_VAL_DATA_PATH=datasets/conll-formatted-ontonotes-5.0/data/development/ allennlp train -s saved_models/srl_5k_glove_wt taskonomy_config/srl_5k_glove_wt.jsonnet
